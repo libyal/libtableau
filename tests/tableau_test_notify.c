0b8c13aa8429af49b78fbb73a07639ddf4e74cf7
Joachim Metz <joachim.metz@gmail.com>
2018-04-03 07:11:50 +0200

    Changes for msvscpp build

--- tests/tableau_test_notify.c
+++ tests/tableau_test_notify.c
@@ -27,6 +27,7 @@
 #include <stdlib.h>
 #endif
 
+#include "tableau_test_libcerror.h"
 #include "tableau_test_libtableau.h"
 #include "tableau_test_macros.h"
 #include "tableau_test_unused.h"
@@ -51,13 +52,37 @@ int tableau_test_notify_set_verbose(
 int tableau_test_notify_set_stream(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libtableau_notify_set_stream(
-	 NULL,
-	 NULL );
+	result = libtableau_notify_set_stream(
+	          NULL,
+	          &error );
+
+	TABLEAU_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	TABLEAU_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+/* TODO test libcnotify_stream_set failure */
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* Tests the libtableau_notify_stream_open function
@@ -66,13 +91,65 @@ int tableau_test_notify_set_stream(
 int tableau_test_notify_stream_open(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libtableau_notify_stream_open(
-	 NULL,
-	 NULL );
+	result = libtableau_notify_stream_open(
+	          "notify_stream.log",
+	          &error );
+
+	TABLEAU_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	TABLEAU_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+	result = libtableau_notify_stream_open(
+	          NULL,
+	          &error );
+
+	TABLEAU_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	TABLEAU_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	/* Clean up
+	 */
+	result = libtableau_notify_stream_close(
+	          &error );
+
+	TABLEAU_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+	TABLEAU_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* Tests the libtableau_notify_stream_close function
@@ -81,12 +158,36 @@ int tableau_test_notify_stream_open(
 int tableau_test_notify_stream_close(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libtableau_notify_stream_close(
-	 NULL );
+	result = libtableau_notify_stream_close(
+	          &error );
+
+	TABLEAU_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+	TABLEAU_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+/* TODO test libcnotify_stream_close failure */
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* The main program
