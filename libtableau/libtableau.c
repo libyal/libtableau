a2e15dcaaa2f8c9ab0140c3483625d53a7f8a1b7
Joachim Metz <joachim.metz@gmail.com>
2020-03-25 06:06:30 +0100

    Applied updates

--- libtableau/libtableau.c
+++ libtableau/libtableau.c
@@ -1,7 +1,7 @@
 /*
  * Library to support the Tableau(TM) forensic bridge (write blocker)
  *
- * Copyright (C) 2008-2019, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2020, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
