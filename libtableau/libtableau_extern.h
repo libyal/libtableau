5398b369fabd67d6d8861afc2d5253b605e4d1cc
Joachim Metz <joachim.metz@gmail.com>
2020-12-31 14:38:47 +0100

    Applied updates

--- libtableau/libtableau_extern.h
+++ libtableau/libtableau_extern.h
@@ -30,7 +30,7 @@
 
 #include <libtableau/extern.h>
 
-#if defined( __CYGWIN__ )
+#if defined( __CYGWIN__ ) || defined( __MINGW32__ )
 #define LIBTABLEAU_EXTERN_VARIABLE	extern
 #else
 #define LIBTABLEAU_EXTERN_VARIABLE	LIBTABLEAU_EXTERN
