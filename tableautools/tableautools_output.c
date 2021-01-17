1fe37d353dc23adfb7d327c2232a89a8c304a6c2
Joachim Metz <joachim.metz@gmail.com>
2021-01-17 12:09:12 +0100

    Applied updates

--- tableautools/tableautools_output.c
+++ tableautools/tableautools_output.c
@@ -1,7 +1,7 @@
 /*
  * Common output functions for the tableautools
  *
- * Copyright (C) 2008-2020, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2021, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -100,7 +100,7 @@ void tableauoutput_copyright_fprint(
 	 */
 	fprintf(
 	 stream,
-	 _( "Copyright (C) 2008-2020, %s.\n" ),
+	 _( "Copyright (C) 2008-2021, %s.\n" ),
 	 _( "Joachim Metz" ) );
 
 	fprintf(
