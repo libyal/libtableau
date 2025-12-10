8b143a22ec200bb47648b59f39e8212e6c9d395a
Joachim Metz <joachim.metz@gmail.com>
2025-12-10 16:30:15 +0100

    Applied updates

--- include/libtableau/handle.h
+++ include/libtableau/handle.h
@@ -1,7 +1,7 @@
 /*
  * The external handle, hides internal data structures
  *
- * Copyright (C) 2008-2024, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2025, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
