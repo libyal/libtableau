e3f705fe86a184c86b6f8ed6efad792cbdb8a7e1
Joachim Metz <joachim.metz@gmail.com>
2026-05-18 06:19:34 +0200

    Applied updates and changes for CI tests

--- libtableau/libtableau_values.h
+++ libtableau/libtableau_values.h
@@ -1,7 +1,7 @@
 /*
  * Tableau bridge values
  *
- * Copyright (C) 2008-2025, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2026, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
